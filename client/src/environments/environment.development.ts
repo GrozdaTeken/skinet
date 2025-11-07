1a1a9043b5e6e01c7b159438f9b3d087422c81fa
GrozdaTeken <aleksandargrozdanic4@gmail.com>
2025-11-07 13:10:58 +0100

    Orders & Payments

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51SI70RQ98worQQbi2K60CdnObiU6PBBBYphjb6honVIrlGcGr0UDVULW47DmU4ENCHlUU18Gm1MGIWqI0f9cVVuA00ludEJR8h'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51SI70RQ98worQQbi2K60CdnObiU6PBBBYphjb6honVIrlGcGr0UDVULW47DmU4ENCHlUU18Gm1MGIWqI0f9cVVuA00ludEJR8h'
 };
